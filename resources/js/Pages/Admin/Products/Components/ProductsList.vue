918f7c9599bcfc6c035ac992138a3010eab90ac2
Rommel <rommel.merca@umtc.com.ph>
2026-03-16 23:06:29 +0800

    adding the products page

--- resources/js/Pages/Admin/Products/Components/ProductsList.vue
+++ resources/js/Pages/Admin/Products/Components/ProductsList.vue
@@ -48,186 +48,230 @@
                     </button>
                 </div>
             </div>
+            <div v-if="products.length < 0"
+                class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-11">
+                <div v-for="product in products" :key="product.id"
+                    class="w-full group relative dark:bg-white rounded-md bg-white border border-gray-100 shadow-sm">
+                    <a href="#" class="flex flex-col items-center bg-neutral-primary-soft p-6 border border-default rounded-base shadow-xs md:flex-row w-full">
+                        <img 
+                            class="object-cover w-full rounded-base h-64 md:h-auto md:w-48 mb-4 md:mb-0" 
+                            :src="product.product_image" 
+                            alt="">
+                        <div class="flex flex-col justify-between md:p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-heading">
+                                Streamlining your design process today.
+                            </h5>
+                            <p class="mb-6 text-body">
+                                In today’s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.
+                            </p>
+                            <div>
+                                <button type="button"
+                                    class="inline-flex items-center w-auto text-body bg-neutral-secondary-medium border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+                                    Read more
+                                </button>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div v-else class="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-11 items-center p-20">
+                <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                    <div class="mr-auto place-self-center lg:col-span-7">
+                        <h1
+                            class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-emerald-900">
+                            No Product Found
+                        </h1>
+                        <p
+                            class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                            Try refreshing the page or using our search box to explore further.
+                        </p>
+                    </div>
+                    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                        <img src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            alt="mockup">
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <script setup>
-    import { router, usePage, Link } from '@inertiajs/vue3';
-    import { ref, onMounted } from 'vue';
-    import axios from 'axios';
-    import { initFlowbite } from 'flowbite';
-    import { defineEmits } from 'vue';
+import { router, usePage, Link } from '@inertiajs/vue3';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { initFlowbite } from 'flowbite';
+import { defineEmits } from 'vue';
 
-    defineProps({
-        products: Array,
-    });
+defineProps({
+    products: Array,
+});
 
-    onMounted(() => {
-        initFlowbite();
+onMounted(() => {
+    initFlowbite();
 
-        const currentRoute = usePage().url;
-        const hasQueryParams = currentRoute.includes('?');
+    const currentRoute = usePage().url;
+    const hasQueryParams = currentRoute.includes('?');
 
-        if (hasQueryParams) {
-            router.visit(currentRoute.split('?')[0], {
-                method: 'get',
-                data: {},
-                preserveState: false,
-                replace: true,
-            });
-        }
-    });
-
-    const dialogVisible              = ref(false);
-    const isLoading                  = ref(false);
-    const selectedBranchId           = ref(null);
-    const bookings                   = ref([]);
-    const salesOrders                = ref([]);
-    const searchQuery                = ref('');
-    const emit                       = defineEmits(['branchRemoved']);
-
-    const openTransactionsModal = (branchId) => {
-        selectedBranchId.value = branchId;
-        dialogVisible.value = true;
-        isLoading.value     = true;
-
-        axios
-            .get(route('admin.branches.branchTransactions'), {
-                params: { branch_id: branchId },
-            })
-            .then((response) => {
-                bookings.value = response.data.bookings;
-            });
-
-        axios
-            .get(route('admin.branches.branchSales'), {
-                params: { branch_id: branchId },
-            })
-            .then((response) => {
-                salesOrders.value = response.data.salesOrders;
-            });
-    };
-
-    const handleClose = () => {
-        dialogVisible.value              = false;
-        isLoading.value                  = false;
-        bookings.value                   = [];
-        salesOrders.value                = [];
-    };
-
-    function refreshProductsData() {
-        if (isLoading.value) return;
-
-        isLoading.value = true;
-        searchQuery.value = '';
-
-        router.visit(route('admin.products.index'), {
+    if (hasQueryParams) {
+        router.visit(currentRoute.split('?')[0], {
             method: 'get',
-            data: {
-            },
-            preserveState: true,
+            data: {},
+            preserveState: false,
             replace: true,
-            onSuccess: () => {
-                isLoading.value = false;
-                Swal.fire({
-                    toast: true,
-                    icon: "success",
-                    position: "top-end",
-                    showConfirmButton: false,
-                    timer: 3000,
-                    title: "Data refreshed!",
-                });
-            },
-            onFinish: () => {
-                isLoading.value = false;
-            },
         });
     }
+});
 
-    const handleSearch = () => {
-        if (isLoading.value) return;
+const dialogVisible = ref(false);
+const isLoading = ref(false);
+const selectedBranchId = ref(null);
+const bookings = ref([]);
+const salesOrders = ref([]);
+const searchQuery = ref('');
+const emit = defineEmits(['branchRemoved']);
 
-        isLoading.value = true;
+const openTransactionsModal = (branchId) => {
+    selectedBranchId.value = branchId;
+    dialogVisible.value = true;
+    isLoading.value = true;
 
-        if (searchQuery.value == '') {
+    axios
+        .get(route('admin.branches.branchTransactions'), {
+            params: { branch_id: branchId },
+        })
+        .then((response) => {
+            bookings.value = response.data.bookings;
+        });
+
+    axios
+        .get(route('admin.branches.branchSales'), {
+            params: { branch_id: branchId },
+        })
+        .then((response) => {
+            salesOrders.value = response.data.salesOrders;
+        });
+};
+
+const handleClose = () => {
+    dialogVisible.value = false;
+    isLoading.value = false;
+    bookings.value = [];
+    salesOrders.value = [];
+};
+
+function refreshProductsData() {
+    if (isLoading.value) return;
+
+    isLoading.value = true;
+    searchQuery.value = '';
+
+    router.visit(route('admin.products.index'), {
+        method: 'get',
+        data: {
+        },
+        preserveState: true,
+        replace: true,
+        onSuccess: () => {
             isLoading.value = false;
             Swal.fire({
                 toast: true,
-                icon: "warning",
+                icon: "success",
                 position: "top-end",
                 showConfirmButton: false,
                 timer: 3000,
-                title: "Please input your prompt on the search box first.",
+                title: "Data refreshed!",
             });
-            return;
-        }
+        },
+        onFinish: () => {
+            isLoading.value = false;
+        },
+    });
+}
 
-        router.visit(route('admin.products.index'), {
-            method: 'get',
-            data: {
-                search: searchQuery.value
-            },
-            preserveState: true,
-            replace: true,
-            onSuccess: () => {
-                isLoading.value = false;
-            },
-            onFinish: () => {
-                isLoading.value = false;
-            },
-        });
-    };
+const handleSearch = () => {
+    if (isLoading.value) return;
 
-    const removeBranch = (branch) => {
-        if (isLoading.value) return;
+    isLoading.value = true;
 
-        isLoading.value = true;
-
+    if (searchQuery.value == '') {
+        isLoading.value = false;
         Swal.fire({
-            title: "Are you sure?",
-            text: `You want to remove branch ${branch.branch_name} from your branch list?`,
-            showCancelButton: true,
-            confirmButtonColor: "#18634c",
-            cancelButtonColor: "#999e9c",
-            confirmButtonText: "Yes, remove it!",
-            cancelButtonText: "Cancel",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                axios
-                    .post(route('admin.branches.remove'), { branch_id: branch.branch_id })
-                    .then((response) => {
-                        if (response.data.success) {
-                            Swal.fire({
-                                toast: true,
-                                icon: "success",
-                                position: "top-end",
-                                showConfirmButton: false,
-                                timer: 3000,
-                                title: "Branch Removed!",
-                            });
-                            emit('branchRemoved');
-                        } else {
-                            Swal.fire({
-                                icon: "error",
-                                title: "Error",
-                                text: response.data.message,
-                            });
-                        }
-                    })
-                    .catch((error) => {
-                        Swal.fire({
-                            title: "Error",
-                            text: "Something went wrong while removing the branch.",
-                        });
-                        console.error(error);
-                    })
-                    .finally(() => {
-                        isLoading.value = false;
-                    });
-            } else {
-                isLoading.value = false;
-            }
+            toast: true,
+            icon: "warning",
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            title: "Please input your prompt on the search box first.",
         });
-    };
+        return;
+    }
+
+    router.visit(route('admin.products.index'), {
+        method: 'get',
+        data: {
+            search: searchQuery.value
+        },
+        preserveState: true,
+        replace: true,
+        onSuccess: () => {
+            isLoading.value = false;
+        },
+        onFinish: () => {
+            isLoading.value = false;
+        },
+    });
+};
+
+const removeBranch = (branch) => {
+    if (isLoading.value) return;
+
+    isLoading.value = true;
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: `You want to remove branch ${branch.branch_name} from your branch list?`,
+        showCancelButton: true,
+        confirmButtonColor: "#18634c",
+        cancelButtonColor: "#999e9c",
+        confirmButtonText: "Yes, remove it!",
+        cancelButtonText: "Cancel",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            axios
+                .post(route('admin.branches.remove'), { branch_id: branch.branch_id })
+                .then((response) => {
+                    if (response.data.success) {
+                        Swal.fire({
+                            toast: true,
+                            icon: "success",
+                            position: "top-end",
+                            showConfirmButton: false,
+                            timer: 3000,
+                            title: "Branch Removed!",
+                        });
+                        emit('branchRemoved');
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "Error",
+                            text: response.data.message,
+                        });
+                    }
+                })
+                .catch((error) => {
+                    Swal.fire({
+                        title: "Error",
+                        text: "Something went wrong while removing the branch.",
+                    });
+                    console.error(error);
+                })
+                .finally(() => {
+                    isLoading.value = false;
+                });
+        } else {
+            isLoading.value = false;
+        }
+    });
+};
 </script>
